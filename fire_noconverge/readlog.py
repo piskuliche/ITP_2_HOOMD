75612a56e1031fea3b5c32e07f02aa2c045d55cd
Zeke Piskulich <piskulichz@gmail.com>
2022-02-17 15:53:17 -0500

    removed

--- fire_noconverge/readlog.py
+++
@@ -1,7 +0,0 @@
-import hoomd
-import gsd.hoomd
-import numpy as np
-
-traj = gsd.hoomd.open('log.gsd', 'rb')
-for frame in traj:
-    print(frame.log['md/compute/ThermodynamicQuantities/potential_energy'])
