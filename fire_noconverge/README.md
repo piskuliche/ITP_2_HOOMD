c2383f615214fba79e262da86b179745ede6b6a1
Zeke Piskulich <piskulichz@gmail.com>
2022-02-17 15:49:25 -0500

    Added subdir to link to hoomd developers

---
+++ fire_noconverge/README.md
@@ -0,0 +1,2 @@
+# ITP_2_HOOMD
+[Work in Progress] A set of python programs to convert a gromacs ITP style (in the OPLS AA format) to a HOOMD simulation.
--- fire_noconverge/save2_hoomd.py
+++ fire_noconverge/save2_hoomd.py
@@ -159,19 +159,15 @@ def gen_pair(ff):
             params[entry]={"sigma":mix_geometric(ff.sigma[ai],ff.sigma[aj]),"epsilon":mix_geometric(ff.epsilon[ai],ff.sigma[aj])}
     return params
 
-def Generate_Snapshot(mol_ff,forcefield,sysinfo):
-    """
-    This function creates an initial configuration and builds a gsd snapshot object. It writes it to the file 'molecular.gsd', which can then be saved.
-    Inputs:
-        -mol_ff: molecule_info class object
-        -forcefield: forcefield_info class object
-        -sysinfo: dictionary with molecule names as keys, and numbers of molecules.
-    """
+def Generate_Snapshot(snapshot,mol_ff,forcefield,sysinfo):
     # Converts charges to proper internal units
     charge_conv = 0.0848385920
 
     # Initialize a new snapshot
     snapshot = gsd.hoomd.Snapshot()
+
+
+
 
     # Set number of atoms based off of gro file
     snapshot.particles.N = N
@@ -194,37 +190,76 @@ def Generate_Snapshot(mol_ff,forcefield,sysinfo):
     snapshot.angles  = set_snap(snapshot.angles, mol_ff, mol_ff.angles,sysinfo["DOPC"])
     snapshot.dihedrals = set_snap(snapshot.dihedrals, mol_ff, mol_ff.dihedrals,sysinfo["DOPC"])
 
+    special_pairs(ff["DOPC"])
+    # Save snapshot
+    with gsd.hoomd.open(name='molecular.gsd', mode='wb') as f:
+        f.append(snapshot)
+    return
+
+
+if __name__ == "__main__":
+    # Converts charges to proper internal units
+    charge_conv = 0.0848385920
+
+    ff ={}
+
+    # Initialize a new snapshot
+    snapshot = gsd.hoomd.Snapshot()
+    
+    # Read the # of atoms, and the positions
+    N,r,names = read_gro("test.gro")
+
+    # Read in molecular force field
+    ff["DOPC"] = read_mol_ff("DOPC-OPLS.itp")
+
+    sysinfo={"DOPC":72}
+
+    # Read in total forcefield
+    forcefield = read_ff_itp("OPLSAA.itp")
+
+    # Set number of atoms based off of gro file
+    snapshot.particles.N = N
+
+    # Initialize particle positions
+    snapshot.particles.position = r
+    setcharge = lambda i: ff["DOPC"].atoms[i].charge*charge_conv
+    snapshot.particles.charge = np.array(list(map(setcharge,names)))
+    
+    # Initialize particle types, find unique types, and assign type ids
+    # These use the map function to get arrays of all the types
+    snapshot.particles = set_part(snapshot.particles,ff["DOPC"]) 
+
+    # Set Box Size
+    snapshot.configuration.box = [200, 200, 200, 0, 0, 0]
+
+    # Set bonds,angles,dihedrals between atoms
+    snapshot.pairs = set_snap(snapshot.pairs,ff["DOPC"], ff["DOPC"].bonds,sysinfo["DOPC"])
+    snapshot.bonds = set_snap(snapshot.bonds, ff["DOPC"], ff["DOPC"].bonds,sysinfo["DOPC"])
+    snapshot.angles  = set_snap(snapshot.angles, ff["DOPC"], ff["DOPC"].angles,sysinfo["DOPC"])
+    snapshot.dihedrals = set_snap(snapshot.dihedrals, ff["DOPC"], ff["DOPC"].dihedrals,sysinfo["DOPC"])
+
+    special_pairs(ff["DOPC"])
     # Save snapshot
     with gsd.hoomd.open(name='molecular.gsd', mode='wb') as f:
         f.append(snapshot)
 
-    return snapshot
-
-def Generate_Forces(snapshot,forcefieldi,r_cut=1.2,buffer=0.1,alpha=1,coul_order=6,res=64):
-    """
-    This function generates the forces object by pulling together all of the forces objects and calling them from the forcefield.
-    Inputs:
-        -snapshot object: GSD snapshot object that has been previously generated, for example by Generate_Snapshot
-        -forcefield: forcefield_info class object.
-    Outputs:
-        -all_forces: a list of hoomd.md.XXX objects where XXX stands for the different types of forces in the system.
-    """
+    
+    # Below this point, only FF information should be generated. 
     nl = hoomd.md.nlist.Cell(buffer=0.1)
     lj = hoomd.md.pair.LJ(nl, default_r_cut=1.2)
     lj.params = gen_pair(forcefield)
-    ew,coul = hoomd.md.long_range.pppm.make_pppm_coulomb_forces(nl, resolution=(res,res,res),order=coul_order,r_cut=r_cut)
+    ew,coul = hoomd.md.long_range.pppm.make_pppm_coulomb_forces(nl, resolution=(64,64,64),order=6,r_cut=1.2)
 
     # Setup HOOMD Simulation
-    # Special Pair Potential 
+    # Bond Potential
     spec_lj_forces = hoomd.md.special_pair.LJ()
     spec_coul_forces = hoomd.md.special_pair.Coulomb()
     for pair in snapshot.pairs.types:
         ptuple = tuple(pair.split("-"))
         spec_lj_forces.params[pair] = lj.params[ptuple]
-        spec_lj_forces.r_cut[pair] = r_cut
-        spec_coul_forces.params[pair] = dict(alpha=alpha)
-        spec_coul_forces.r_cut[pair] = r_cut
-    # Bond Potential
+        spec_lj_forces.r_cut[pair] = 1.2
+        spec_coul_forces.params[pair] = dict(alpha=1)
+        spec_coul_forces.r_cut[pair] = 1.2
     bondforces = hoomd.md.bond.Harmonic()
     for bond in snapshot.bonds.types:
         btuple = tuple(bond.split("-"))
@@ -243,59 +278,20 @@ def Generate_Forces(snapshot,forcefieldi,r_cut=1.2,buffer=0.1,alpha=1,coul_order
         dvals = forcefield.dihedraltypes[dtuple]
         dihedralforces.params[dih] = RB_to_OPLS(dvals)
 
-    all_forces=[ew,coul,spec_lj_forces,spec_coul_forces,bondforces,angleforces,dihedralforces]
-    return all_forces
-
-
-if __name__ == "__main__":
-    # Converts charges to proper internal units
-    charge_conv = 0.0848385920
-    # Read the # of atoms, and the positions
-    N,r,names = read_gro("test.gro")
-    # Read in molecular force field
-    ff = {}
-    ff["DOPC"] = read_mol_ff("DOPC-OPLS.itp")
-    sysinfo={"DOPC":72}
-
-    # Read in total forcefield
-    forcefield = read_ff_itp("OPLSAA.itp")
-
-    # Generate Snapshot
-    snapshot=Generate_Snapshot(ff["DOPC"],forcefield,sysinfo)
-   
-    # Generate Forces
-    all_forces = Generate_Forces(snapshot,forcefield)
-
     # Perform the MD simulation.
     sim = hoomd.Simulation(device=hoomd.device.CPU(), seed=1)
     sim.create_state_from_gsd(filename='molecular.gsd')
-
-    # Set initial velocities
     sim.state.thermalize_particle_momenta(filter=hoomd.filter.All(), kT=2.5)
-    fire = hoomd.md.minimize.FIRE(dt=0.05,
-                            force_tol=1e-2,
-                            angmom_tol=1e-2,
-                            energy_tol=1e-7)
-    fire.methods.append(hoomd.md.methods.NVE(hoomd.filter.All()))
-    sim.operations.integrator = fire
-    fire.converged()
-    while not(fire.converged):
-        sim.run(100)
-
-    #ensemble = hoomd.md.methods.NPT(filter=hoomd.filter.All(),kT=2.5,tau=1, tauS=10, S=[1/16,1/16,1/16,0,0,0],couple='xy')
-    ensemble = hoomd.md.methods.NVE(filter=hoomd.filter.All())
+    ensemble = hoomd.md.methods.NPT(filter=hoomd.filter.All(),kT=2.5,tau=1, tauS=10, S=[1/16,1/16,1/16,0,0,0],couple='xy')
+    #ensemble = hoomd.md.methods.NVE(filter=hoomd.filter.All())
     integrator = hoomd.md.Integrator(dt=0.001,
                                      methods=[ensemble],
-                                     forces=all_forces)
+                                     forces=[spec_lj_forces,spec_coul_forces,ew,coul,bondforces,angleforces,dihedralforces])
     gsd_writer = hoomd.write.GSD(filename='molecular_trajectory.gsd',
                                  trigger=hoomd.trigger.Periodic(1),
                                  mode='wb')
-
-    # Calculate Thermodynamics
     thermodynamic_properties = hoomd.md.compute.ThermodynamicQuantities(filter=hoomd.filter.All())
     sim.operations.computes.append(thermodynamic_properties)
-    
-    # Add A file to Log Thermodynamics
     logger = hoomd.logging.Logger()
     logger.add(thermodynamic_properties)
 
@@ -303,8 +299,6 @@ if __name__ == "__main__":
                              trigger=hoomd.trigger.Periodic(1),
                              mode='wb',
                              filter=hoomd.filter.Null())
-
-    # Final Run
     sim.operations.integrator = integrator
     sim.operations.writers.append(gsd_writer)
     sim.operations.writers.append(gsd_writer2)
